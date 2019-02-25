8c7c67fe36b3d774dd31e84aeb890cd8c9485e89
Youssefares <Youssefares@ymail.com>
2019-02-26 00:54:54 +0200

    Add support for editting all custom user fields

--- app/controllers/overrides/devise_token_auth_overrides/registrations_controller.rb
+++ app/controllers/overrides/devise_token_auth_overrides/registrations_controller.rb
@@ -2,12 +2,23 @@ module Overrides
   module DeviseTokenAuthOverrides
     # Modified Registrations Controller adding our custom fields to devise
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
-      before_action :permit_extra_params, only: %i[create]
+      before_action :permit_extra_params, only: %i[create update]
+      skip_before_action :validate_account_update_params, only: :update
 
       def permit_extra_params
+        # Mapping from method names given in params
+        # To action name required by devise_parameter_sanitizer.permit.
+        param_to_action_name = {
+          'create' => :sign_up, 'update' => :account_update
+        }
+        action_name = -> { return param_to_action_name[params['action']] }
+
+        # Permit params
         devise_parameter_sanitizer.permit(
-          :sign_up,
-          keys: %i[first_name last_name gender date_of_birth city country]
+          action_name.call,
+          keys: %i[
+            first_name last_name gender date_of_birth city country image bio
+          ]
         )
       end
     end
