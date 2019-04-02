f2f64e381eac680685031220d3d60417566692b4
yara11 <yara.m.abdullatif@gmail.com>
2019-04-02 22:21:08 +0200

    created user#show and user#contribution routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[show contributions]
+  authorize_resource
+
+  def show
+    render json: UserSerializer.new(current_user).serialized_json,
+           status: :ok
+  end
+
+  def contributions
+    per_page = params[:per_page] || 5
+    page = params[:page] || 1
+
+    render json: PaginatedSerializableService.new(
+      records: current_user.gestures,
+      serializer_klass: GestureSerializer,
+      page: page,
+      per_page: per_page
+    ).build_hash, status: :ok
+  end
+end
