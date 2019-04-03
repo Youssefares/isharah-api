1b5f5cb311fc6c66af86ad40352a567bb68b18b7
yara11 <yara.m.abdullatif@gmail.com>
2019-04-03 18:07:45 +0200

    add ancestors and descendants in category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,18 @@
 class CategorySerializer
   include FastJsonapi::ObjectSerializer
   attribute :name
-  # TODO: add category ancestors and descendents here someway
+
+  has_one :parent do |category|
+  	category.parent
+  end
+  has_many :ancestors do |category|
+  	category.ancestors
+  end
+  has_many :descendants do |category|
+  	category.descendants
+  end
+  has_many :children do |category|
+  	category.children
+  end
+
 end
