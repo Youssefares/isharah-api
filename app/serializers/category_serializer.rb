56641ece50a474cc1bdf26e5a0da343f1c6a3b03
yara11 <yara.m.abdullatif@gmail.com>
2019-04-09 13:47:28 +0200

    amend requested changes

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,16 +2,8 @@ class CategorySerializer
   include FastJsonapi::ObjectSerializer
   attribute :name
 
-  has_one :parent do |category|
-    category.parent
-  end
-  has_many :ancestors do |category|
-    category.ancestors
-  end
-  has_many :descendants do |category|
-    category.descendants
-  end
-  has_many :children do |category|
-    category.children
-  end
+  has_one :parent
+  has_many :ancestors
+  has_many :descendants
+  has_many :children
 end
