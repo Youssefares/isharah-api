539c72b3cd644a670e27bdeffe7ad1497a2fc62d
Youssefares <Youssefares@ymail.com>
2019-04-10 02:06:29 +0200

    Modify category serializer to not include relations by default

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,8 +2,16 @@ class CategorySerializer
   include FastJsonapi::ObjectSerializer
   attribute :name
 
-  has_one :parent, serializer: :category
-  has_many :ancestors, serializer: :category
-  has_many :descendants, serializer: :category
-  has_many :children, serializer: :category
+  has_one :parent, serializer: :category, if: proc { |_, params|
+    params && params[:include_relations] == true
+  }
+  has_many :ancestors, serializer: :category, if: proc { |_, params|
+    params && params[:include_relations] == true
+  }
+  has_many :descendants, serializer: :category, if: proc { |_, params|
+    params && params[:include_relations] == true
+  }
+  has_many :children, serializer: :category, if: proc { |_, params|
+    params && params[:include_relations] == true
+  }
 end
