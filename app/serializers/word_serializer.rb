59da3e91254286c967cb9c4b56380d8f41833f45
Youssefares <Youssefares@ymail.com>
2019-04-15 01:02:35 +0200

    Modify word serialization to include right type for public_dictionary_gesture, Update swagger Word endpoints

--- app/serializers/word_serializer.rb
+++ app/serializers/word_serializer.rb
@@ -3,6 +3,7 @@ class WordSerializer
   attributes :name, :part_of_speech
 
   has_one :primary_dictionary_gesture,
+          record_type: :gesture,
           serializer: GestureSerializer,
           if: proc { |_, params| params && params[:include_gesture] == true }
   has_many :categories
