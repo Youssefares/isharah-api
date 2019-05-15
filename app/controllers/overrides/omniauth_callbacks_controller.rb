34965ad84c993d962d78a0620a8bfba62eddd70f
yara11 <yara.m.abdullatif@gmail.com>
2019-05-15 21:47:19 +0200

    modify callbacks controller

--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 module Overrides
   class OmniauthCallbacksController <
     DeviseTokenAuth::OmniauthCallbacksController
+    # https://github.com/lynndylanhurley/devise_token_auth/issues/1020
     def assign_provider_attrs(user, auth_hash)
       all_attrs = auth_hash['info'].slice(*user.attributes.keys)
       orig_val = ActionController::Parameters.permit_all_parameters
@@ -10,41 +11,6 @@ module Overrides
       user.assign_attributes(permitted_attrs)
       ActionController::Parameters.permit_all_parameters = orig_val
       user
-    end
-
-    def get_resource_from_auth_hash # rubocop:disable Naming/AccessorMethodName
-      logger.debug auth_hash
-      # find by provider and provider uid
-      @resource = resource_class.find_by(
-        uid: auth_hash['uid'],
-        provider: auth_hash['provider']
-      )
-
-      if @resource.nil? # New user
-        @resource = User.new
-        set_resource_fields
-        @oauth_registration = true
-      end
-
-      # sync user info with provider, update/generate auth token
-      assign_provider_attrs(@resource, auth_hash)
-
-      # assign any additional (whitelisted) attributes
-      extra_params = whitelisted_params
-      @resource.assign_attributes(extra_params) if extra_params
-
-      @resource
-    end
-
-    def set_resource_fields
-      # TODO: Add city, country and DoB
-      @resource.assign_attributes(
-        uid: auth_hash['uid'],
-        provider: auth_hash['provider'],
-        email: auth_hash['info']['email'],
-        first_name: auth_hash['info']['first_name'],
-        last_name: auth_hash['info']['last_name']
-      )
     end
   end
 end
