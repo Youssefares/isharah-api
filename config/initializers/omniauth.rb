832ba7c58c6c2ed9b974e6c05f441b6cdfebd32d
Yara Abdullatif <yara.m.abdullatif@gmail.com>
2019-05-17 20:07:08 +0200

    added google authentication (#53)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], 
   scope: 'email', display: 'popup',
   info_fields: 'email,first_name,last_name', locale: 'ar'
+
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
