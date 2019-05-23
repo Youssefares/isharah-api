ce3f6b6336f276ac2e0036a76bb0b490bec164b7
Youssefares <Youssefares@ymail.com>
2019-05-24 01:03:53 +0200

    Fix bug in video trimming service, Update create gesture endpoint in swagger

--- app/services/video_trimmer_service.rb
+++ app/services/video_trimmer_service.rb
@@ -4,7 +4,7 @@ class VideoTrimmerService
     tmp_file_name = "#{input_path}_temp"
     @command = [
       'ffmpeg', '-i', input_path.to_s, '-f', 'mp4', '-ss', start.to_s,
-      '-t', finish.to_s, '-async', '1', '-f', 'mp4', tmp_file_name.to_s, '-y'
+      '-to', finish.to_s, '-async', '1', '-f', 'mp4', tmp_file_name.to_s, '-y'
     ]
     @cleanup_command = ['mv', tmp_file_name.to_s, input_path.to_s]
   end
