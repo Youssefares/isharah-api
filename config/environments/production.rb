ba9200f07427335046b5812da4e3d7513f8b0cb5
Youssefares <Youssefares@ymail.com>
2019-05-23 20:50:01 +0200

    Add missing port in actionmailer default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # TODO: fix this when we go to broad
-  config.action_mailer.default_url_options = { host: 'isharah.com' }
+  config.action_mailer.default_url_options = { host: 'isharah.com', port: 8888 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
